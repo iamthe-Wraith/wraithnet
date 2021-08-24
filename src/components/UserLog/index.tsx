06e3da3fa8deee2de3999af55561030c98085240
iamthe-Wraith <allhailthewraith@protonmail.com>
2021-08-24 08:15:57 -0400

    adds support for changing dates to view user log entries

--- src/components/UserLog/index.tsx
+++ src/components/UserLog/index.tsx
@@ -3,30 +3,72 @@ import { observer } from 'mobx-react';
 import React, { useEffect, useRef, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { UserLogsModel } from '../../models/userLogs';
+import { Button, ButtonType } from '../Button';
+import { NextArrowIcon } from '../icons/NextArrowIcon';
+import { PrevArrowIcon } from '../icons/PrevArrowIcon';
 import { UserLogEntry } from '../UserLogEntry';
 
-import { UserLogContainer, UserLogEntries, UserLogHeader } from './styles';
+import {
+    DateContainer,
+    NoEntries,
+    UserLogContainer,
+    UserLogEntries,
+    UserLogHeader
+} from './styles';
 
 interface IProps {
     className?: string;
 }
 
+enum DateChangeDirection {
+    Next = 'next',
+    Prev = 'prev',
+}
+
 const UserLogBase: React.FC<IProps> = ({ className = '' }) => {
     const userLogsModel = useRef(new UserLogsModel()).current;
-    const [selectedDate, setSelectedDte] = useState(dayjs().local());
+    const [selectedDate, setSelectedDate] = useState(dayjs().local());
 
     useEffect(() => {
+        console.log('selectedDate change detected');
         userLogsModel.setCriteria({ created: selectedDate.format() });
-    }, []);
+    }, [selectedDate]);
+
+    const onDateChangeClick = (direction: DateChangeDirection) => () => {
+        setSelectedDate(selectedDate[direction === DateChangeDirection.Next ? 'add' : 'subtract'](1, 'day'));
+    }
 
     const renderEntries = () => {
+        if (userLogsModel.entries.length === 0) {
+            return (
+                <NoEntries>
+                    No entries found
+                </NoEntries>
+            );
+        }
+
         return userLogsModel.entries.map(entry => <UserLogEntry key={ entry.id } entry={ entry } />);
     }
 
     return (
         <UserLogContainer>
             <UserLogHeader>
-                <span>{ selectedDate.format('MMM DD, YYYY') }</span>
+                <DateContainer>
+                    <Button
+                        onClick={ onDateChangeClick(DateChangeDirection.Prev) }
+                        type={ ButtonType.Blank }
+                    >
+                        <PrevArrowIcon className='date-arrow-button' />
+                    </Button>
+                    <span>{ selectedDate.format('MMM DD, YYYY') }</span>
+                    <Button
+                        onClick={ onDateChangeClick(DateChangeDirection.Next) }
+                        type={ ButtonType.Blank }
+                    >
+                        <NextArrowIcon className='date-arrow-button' />
+                    </Button>
+                </DateContainer>
+                <span>entries: { userLogsModel.count }</span>
                 <Link to='/'>close</Link>
             </UserLogHeader>
             <UserLogEntries>
--- src/components/UserLog/styles.tsx
+++ src/components/UserLog/styles.tsx
@@ -1,18 +1,35 @@
 import styled from 'styled-components';
 import { ITheme } from '../../styles/themes';
 
+export const DateContainer = styled.div`
+    align-items: center;
+    display: flex;
+    justify-content: space-between;
+    width: 200px;
+
+    .date-arrow-button {
+        height: 24px;
+        width: 24px;
+    }
+`;
+
+export const NoEntries = styled.div`
+    color: ${({theme}: { theme: ITheme}) => theme.gray};
+    margin-top: 20px;
+`;
+
 export const UserLogContainer = styled.div`
     height: 100%;
 `;
 
 export const UserLogEntries = styled.div`
     overflow: auto;
-    height: calc(100% - 30px);
+    height: calc(100% - 40px);
 `;
 
 export const UserLogHeader = styled.div`
     color: ${({theme}: { theme: ITheme }) => theme.primary};
     display: flex;
     justify-content: space-between;
-    height: 30px;
+    height: 40px;
 `;
