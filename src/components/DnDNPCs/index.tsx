e64cc4a9011eb270d69ddacb8c5b9f86dc40d9bb
iamthe-Wraith <allhailthewraith@protonmail.com>
2021-12-01 08:01:19 -0500

    fixes selected tag bug
    Closes https://github.com/iamthe-Wraith/wraithnet/issues/108

--- src/components/DnDNPCs/index.tsx
+++ src/components/DnDNPCs/index.tsx
@@ -4,30 +4,30 @@ import { DnDContext } from '../../contexts/DnD';
 import { CollectionNoteEditor } from '../CollectionNoteEditor';
 
 interface IProps {
-    className?: string;
+  className?: string;
 }
 
 const DnDNPCsBase: React.FC<IProps> = ({ className = '' }) => {
-    const dnd = useContext(DnDContext);
+  const dnd = useContext(DnDContext);
 
-    const onCreateNewNpcClick = async (name: string) => {
-        try {
-            return await dnd.campaign.createNPC(name);
-        } catch (err) {
-            console.log('error creating npc');
-            console.log(err);
-        }
+  const onCreateNewNpcClick = async (name: string) => {
+    try {
+      return await dnd.campaign.createNPC(name);
+    } catch (err) {
+      console.log('error creating npc');
+      console.log(err);
     }
+  };
 
-    return (
-        <CollectionNoteEditor
-            busy={ dnd.campaign.busy }
-            className={ className }
-            collection={ dnd.campaign.npcs }
-            onCreateNewClick={ onCreateNewNpcClick }
-            type='NPC'
-        />
-    );
+  return (
+    <CollectionNoteEditor
+      busy={ dnd.campaign.busy }
+      className={ className }
+      collection={ dnd.campaign.npcs }
+      onCreateNewClick={ onCreateNewNpcClick }
+      type='NPC'
+    />
+  );
 };
 
 export const DnDNPCs = observer(DnDNPCsBase);
--- src/components/TagsList/index.tsx
+++ src/components/TagsList/index.tsx
@@ -1,9 +1,8 @@
 import { observer } from 'mobx-react';
 import React, { FC, useEffect, useRef, useState } from 'react';
 import { Waypoint } from 'react-waypoint';
-import { CollectionModel } from '../../models/collection';
-import { ITag, TagModel, TagsModel } from '../../models/tags';
-import { Button, ButtonType } from '../Button';
+import { TagModel, TagsModel } from '../../models/tags';
+import { ButtonType } from '../Button';
 import { Checkbox } from '../Checkbox';
 import { CTAs } from '../CtasContainer';
 import { LoadingSpinner, SpinnerSize, SpinnerType } from '../LoadingSpinner';
@@ -13,182 +12,182 @@ import { TextInput } from '../TextInput';
 import { LoadingSpinnerContainer, NewTagModal, NoTagsContainer, TagContainer, TagsListContainer, TagsListItems } from './styles';
 
 interface IProps {
-    className?: string;
-    defaultSelectedTags?: TagModel[];
-    forceClearSelectedList?: boolean;
-    onSelectedTagsChange?: (selectedTags: TagModel[]) => void;
+  className?: string;
+  defaultSelectedTags?: TagModel[];
+  forceClearSelectedList?: boolean;
+  onSelectedTagsChange?: (selectedTags: TagModel[]) => void;
 }
 
 const TagsListBase: FC<IProps> = ({ className, defaultSelectedTags = [], forceClearSelectedList, onSelectedTagsChange }) => {
-    const tagsModel = useRef(new TagsModel()).current;
-    const [selectedTags, setSelectedTags] = useState<TagModel[]>(defaultSelectedTags);
-    const [showTagModal, setShowTagModal] = useState(false);
-    const [newTagName, setNewTagName] = useState('');
-    const tagsEngaged = useRef(false);
+  const tagsModel = useRef(new TagsModel()).current;
+  const [selectedTags, setSelectedTags] = useState<TagModel[]>(defaultSelectedTags);
+  const [showTagModal, setShowTagModal] = useState(false);
+  const [newTagName, setNewTagName] = useState('');
+  const tagsEngaged = useRef(false);
 
-    const loadMore = () => {
-        // tagsModel.getTags(true);
-        tagsModel.tags.loadMore()
-            .catch(err => {
-                console.log('error loading tags');
-                console.error(err);
-            });
-    };
+  const loadMore = () => {
+    // tagsModel.getTags(true);
+    tagsModel.tags.loadMore()
+      .catch(err => {
+        console.log('error loading tags');
+        console.error(err);
+      });
+  };
 
-    useEffect(() => {
-        selectedTags.map(tag => tagsModel.tags.unshift(tag));
-        loadMore();
-    }, []);
+  useEffect(() => {
+    selectedTags.map(tag => tagsModel.tags.unshift(tag));
+    loadMore();
+  }, []);
 
-    useEffect(() => {
-        if (forceClearSelectedList) {
-            setSelectedTags([]);
-        }
-    }, [forceClearSelectedList]);
-
-    useEffect(() => {
-        if (tagsEngaged.current) {
-            onSelectedTagsChange?.(selectedTags);
-        }
-    }, [selectedTags]);
-
-    const onCreateClick = (name: string) => () => {
-        tagsModel.createTag(name)
-            .then(tag => {
-                setSelectedTags([...selectedTags, tag]);
-                setShowTagModal(false);
-            })
-            .catch(err => {
-                console.log('error creating tag');
-                console.log(err);
-            });
+  useEffect(() => {
+    if (forceClearSelectedList) {
+      setSelectedTags([]);
     }
+  }, [forceClearSelectedList]);
 
-    const onTagChange = (tag: TagModel) => (e: React.ChangeEvent<HTMLInputElement>) => {
-        tagsEngaged.current = true;
-        const isSelected = selectedTags.find(selectedTag => selectedTag.id === tag.id);
-
-        if (isSelected) {
-            setSelectedTags(selectedTags.filter(selectedTag => selectedTag.id !== tag.id));
-        } else {
-            setSelectedTags([...selectedTags, tag]);
-        }
+  useEffect(() => {
+    if (tagsEngaged.current) {
+      onSelectedTagsChange?.(selectedTags);
     }
+  }, [selectedTags]);
 
-    const renderTags = () => {
-        if (tagsModel.tags.allResultsFetched && tagsModel.tags.results.length === 0) {
-            return (
-                <NoTagsContainer>
+  const onCreateClick = (name: string) => () => {
+    tagsModel.createTag(name)
+      .then(tag => {
+        setSelectedTags([...selectedTags, tag]);
+        setShowTagModal(false);
+      })
+      .catch(err => {
+        console.log('error creating tag');
+        console.log(err);
+      });
+  };
+
+  const onTagChange = (tag: TagModel) => () => {
+    tagsEngaged.current = true;
+    const isSelected = selectedTags.find(selectedTag => selectedTag.id === tag.id);
+
+    if (isSelected) {
+      setSelectedTags(selectedTags.filter(selectedTag => selectedTag.id !== tag.id));
+    } else {
+      setSelectedTags([...selectedTags, tag]);
+    }
+  };
+
+  const renderTags = () => {
+    if (tagsModel.tags.allResultsFetched && tagsModel.tags.results.length === 0) {
+      return (
+        <NoTagsContainer>
                     No tags found
-                </NoTagsContainer>
-            )
-        }
-
-        const tags = tagsModel.tags.results.map(t => {
-            const isChecked = !!selectedTags.find(selectedTag => selectedTag.id === t.id);
-            const tag = (
-                <Tag
-                    allowHoverHighlight={ !!onSelectedTagsChange }
-                    isHighlighted={ isChecked }
-                    text={ t.text }
-                    type={ isChecked ? TagType.Secondary : TagType.Primary }
-                />
-            )
-
-            return (
-                <TagContainer key={ t.text }>
-                    {
-                        !!onSelectedTagsChange
-                            ? (
-                                <Checkbox
-                                    checked={ isChecked }
-                                    id={ `taglist-${t.id}` }
-                                    label={ tag }
-                                    onChange={ onTagChange(t) }
-                                />
-                            )
-                            : tag
-                    }
-                </TagContainer>
-            )
-        });
-
-        if (tagsModel.tags.busy) {
-            const spinner = (
-                <LoadingSpinner
-                    className={ tagsModel.tags.firstPageLoaded ? '' : 'loading-spinner' }
-                    key='loading-spinner'
-                    size={ SpinnerSize.Small }
-                    type={ SpinnerType.Two }
-                />
-            );
-
-            if (tagsModel.tags.firstPageLoaded) {
-                tags.push((
-                    <LoadingSpinnerContainer key='loading-spinner-container'>
-                        { spinner }
-                    </LoadingSpinnerContainer>
-                ))
-            } else {
-                tags.push(spinner);
-            }
-        }
-
-        if (!tagsModel.tags.allResultsFetched && !tagsModel.tags.busy) {            
-            return [...tags, <Waypoint key='waypoint' onEnter={ loadMore } topOffset={ 300 } />];
-        }
-
-        return tags
+        </NoTagsContainer>
+      );
     }
 
-    return (
-        <TagsListContainer className={ className }>
-            <TagsListItems>
-                { renderTags() }
-            </TagsListItems>
-            <CTAs
-                className='ctas'
-                ctas={[
-                    {
-                        text: '+ add tag',
-                        type: ButtonType.Blank,
-                        onClick: () => setShowTagModal(true)
-                    }
-                ]}
-            />
-            <Modal
-                header='New Tag'
-                onClose={() => setShowTagModal(false)}
-                isOpen={ showTagModal }
-                size={ ModalSize.Small }
-            >
-                <NewTagModal>
-                    <div className='label'>tag name</div>
-                    <TextInput
-                        inputId={ `new-tag-input` }
-                        onChange={e => setNewTagName(e.target.value)}
-                        value={ newTagName }
-                    />
-                    <CTAs
-                        ctas={[
-                            {
-                                disabled: !newTagName,
-                                text: 'create',
-                                type: ButtonType.Primary,
-                                onClick: onCreateClick(newTagName),
-                            },
-                            {
-                                text: 'cancel',
-                                type: ButtonType.Blank,
-                                onClick: () => setShowTagModal(false),
-                            }
-                        ]}
-                    />
-                    { tagsModel.creatingTag && <LoadingSpinner size={ SpinnerSize.Tiny } />}
-                </NewTagModal>
-            </Modal>
-        </TagsListContainer>
-    )
+    const tags = tagsModel.tags.results.map(t => {
+      const isChecked = !!selectedTags.find(selectedTag => selectedTag.id === t.id);
+      const tag = (
+        <Tag
+          allowHoverHighlight={ !!onSelectedTagsChange }
+          isHighlighted={ isChecked }
+          text={ t.text }
+          type={ isChecked ? TagType.Secondary : TagType.Primary }
+        />
+      );
+
+      return (
+        <TagContainer key={ t.text }>
+          {
+            !!onSelectedTagsChange
+              ? (
+                <Checkbox
+                  checked={ isChecked }
+                  id={ `taglist-${t.id}` }
+                  label={ tag }
+                  onChange={ onTagChange(t) }
+                />
+              )
+              : tag
+          }
+        </TagContainer>
+      );
+    });
+
+    if (tagsModel.tags.busy) {
+      const spinner = (
+        <LoadingSpinner
+          className={ tagsModel.tags.firstPageLoaded ? '' : 'loading-spinner' }
+          key='loading-spinner'
+          size={ SpinnerSize.Small }
+          type={ SpinnerType.Two }
+        />
+      );
+
+      if (tagsModel.tags.firstPageLoaded) {
+        tags.push((
+          <LoadingSpinnerContainer key='loading-spinner-container'>
+            { spinner }
+          </LoadingSpinnerContainer>
+        ));
+      } else {
+        tags.push(spinner);
+      }
+    }
+
+    if (!tagsModel.tags.allResultsFetched && !tagsModel.tags.busy) {            
+      return [...tags, <Waypoint key='waypoint' onEnter={ loadMore } topOffset={ 300 } />];
+    }
+
+    return tags;
+  };
+
+  return (
+    <TagsListContainer className={ className }>
+      <TagsListItems>
+        { renderTags() }
+      </TagsListItems>
+      <CTAs
+        className='ctas'
+        ctas={ [
+          {
+            text: '+ add tag',
+            type: ButtonType.Blank,
+            onClick: () => setShowTagModal(true),
+          },
+        ] }
+      />
+      <Modal
+        header='New Tag'
+        onClose={ () => setShowTagModal(false) }
+        isOpen={ showTagModal }
+        size={ ModalSize.Small }
+      >
+        <NewTagModal>
+          <div className='label'>tag name</div>
+          <TextInput
+            inputId={ `new-tag-input` }
+            onChange={ e => setNewTagName(e.target.value) }
+            value={ newTagName }
+          />
+          <CTAs
+            ctas={ [
+              {
+                disabled: !newTagName,
+                text: 'create',
+                type: ButtonType.Primary,
+                onClick: onCreateClick(newTagName),
+              },
+              {
+                text: 'cancel',
+                type: ButtonType.Blank,
+                onClick: () => setShowTagModal(false),
+              },
+            ] }
+          />
+          { tagsModel.creatingTag && <LoadingSpinner size={ SpinnerSize.Tiny } />}
+        </NewTagModal>
+      </Modal>
+    </TagsListContainer>
+  );
 };
 
 export const TagsList = observer(TagsListBase);
