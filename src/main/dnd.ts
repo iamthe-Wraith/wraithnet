649ae7b278f01760e7af5e3e981a5e36497b1aed
iamthe-Wraith <allhailthewraith@protonmail.com>
2021-12-07 07:56:38 -0500

    enables devtools for the dnd window

--- src/main/dnd.ts
+++ src/main/dnd.ts
@@ -5,46 +5,46 @@ import Window from '../lib/window';
 import { Base, IBaseProps } from './base';
 
 interface IProps extends IBaseProps {
-    isDev: boolean;
-  }
+  isDev: boolean;
+}
 
 export class DnD extends Base {
-    constructor(props: IProps) {
-        super(props);
-        this._windowName = 'login';
-    }
+  constructor(props: IProps) {
+    super(props);
+    this._windowName = 'login';
+  }
 
-    public init = async () => {
-        if (!this._window) {
-            this._createWindow();
-            this._setListeners();
-        }
+  public init = async () => {
+    if (!this._window) {
+      this._createWindow();
+      this._setListeners();
     }
+  }
 
-    protected _createWindow = () => {
-        this._window = new Window({
-            backgroundColor: '#000',
-            display: 'cursor',
-            filename: path.resolve(__dirname, 'dnd.html'),
-            height: 'full',
-            width: 'full',
-            webPreferences: {
-                devTools: process.env.NODE_ENV === 'development',
-                preload: path.resolve(__dirname, 'dndPreloader.js'),
-            },
-            onClosed: () => {
-                this._shutdownListeners();
-            },
-        });
+  protected _createWindow = () => {
+    this._window = new Window({
+      backgroundColor: '#000',
+      display: 'cursor',
+      filename: path.resolve(__dirname, 'dnd.html'),
+      height: 'full',
+      width: 'full',
+      webPreferences: {
+        devTools: true, // process.env.NODE_ENV === 'development',
+        preload: path.resolve(__dirname, 'dndPreloader.js'),
+      },
+      onClosed: () => {
+        this._shutdownListeners();
+      },
+    });
 
-        this._isOpen = true;
-    }
+    this._isOpen = true;
+  }
 
-    private _setListeners = () => {
-        ipcMain.on('close-dnd', this.close);
-    }
+  private _setListeners = () => {
+    ipcMain.on('close-dnd', this.close);
+  }
 
-    private _shutdownListeners = () => {
-        ipcMain.off('close-dnd', this.close);
-    }
+  private _shutdownListeners = () => {
+    ipcMain.off('close-dnd', this.close);
+  }
 }
