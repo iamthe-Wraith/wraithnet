ceb3edca0c537c736f7874ac7260cb2df35c775d
iamthe-Wraith <allhailthewraith@protonmail.com>
2021-12-07 06:39:21 -0500

    updates to version 0.0.4

--- src/components/DnD/index.tsx
+++ src/components/DnD/index.tsx
@@ -5,49 +5,48 @@ import { UserContext } from '../../contexts/User';
 import { DnDIpcRenderer as IpcRenderer } from '../../models/ipcRenderers/dnd';
 import { Campaign } from '../Campaign';
 import { CampaignsList } from '../CampaignsList';
-import { DnDCampaignRouter } from '../DnDCampaignRouter';
 import { LoadingSpinner } from '../LoadingSpinner';
 
 import { DnDContainer } from './styles';
 
 export const DnDBase: React.FC = () => {
-    const user = useContext(UserContext);
-    const dnd = useContext(DnDContext);
+  const user = useContext(UserContext);
+  const dnd = useContext(DnDContext);
 
-    useEffect(() => {
-        IpcRenderer.init();
+  useEffect(() => {
+    IpcRenderer.init();
 
-        dnd.getCampaigns()
-            .then(() => {
-                // TODO: this is only for development purposes...needs to be removed...
-                dnd.forceSelect();
-            })
-            .catch(err => {
-                console.error(err);
-            });
-    }, []);
+    dnd.getCampaigns()
+      .then(() => {
+        // TODO: this is only for development purposes...needs to be removed...
+        // dnd.forceSelect();
+      })
+      .catch(err => {
+        console.error(err);
+      });
+  }, []);
 
-    if (!user.username) {
-        return (
-            <DnDContainer>
-                <LoadingSpinner className='loading' />
-            </DnDContainer>
-        );
-    }
-
-    const renderContent = () => {
-        if (!dnd.campaign) {
-            return <CampaignsList className='campaigns-list' />;
-        }
-
-        return <Campaign />;
-    }
-
+  if (!user.username) {
     return (
-        <DnDContainer>
-            { renderContent() }
-        </DnDContainer>
-    )
+      <DnDContainer>
+        <LoadingSpinner className='loading' />
+      </DnDContainer>
+    );
+  }
+
+  const renderContent = () => {
+    if (!dnd.campaign) {
+      return <CampaignsList className='campaigns-list' />;
+    }
+
+    return <Campaign />;
+  };
+
+  return (
+    <DnDContainer>
+      { renderContent() }
+    </DnDContainer>
+  );
 };
 
 export const DnD = observer(DnDBase);
