a701570621aca55846b30d2e0f9f0c5613d0eab0
iamthe-Wraith <allhailthewraith@protonmail.com>
2022-01-01 00:18:04 -0500

    updates design and position of toaster

--- src/components/Toaster/index.tsx
+++ src/components/Toaster/index.tsx
@@ -7,34 +7,35 @@ import { observer } from 'mobx-react';
 import { ToastType } from '../../models/toaster';
 
 interface IProps {
-  className?: string;
+    className?: string;
 }
 
-const overlayFrom = { opacity: 0 };
-const overlayTo = { opacity: 1 };
-const toasterFrom = { ...overlayFrom, transform: 'translate3d(-50%, -80%, 0)' };
-const toasterTo = { ...overlayTo, transform: 'translate3d(-50%, -50%, 0)' };
+const toasterFrom = { opacity: 0, transform: 'translate3d(30px, 0, 0)' };
+const toasterTo = { opacity: 1, transform: 'translate3d(0, 0, 0)' };
 
 const ToasterBase: React.FC<IProps> = ({
-  className = '',
+    className = '',
 }) => {
-  const toasterModel = useContext(ToasterContext);
-  const toasterSpring = useSpring({
-    config: config.gentle,
-    from: toasterFrom,
-    to: !!toasterModel.currentToast ? toasterTo : toasterFrom, 
-  });
+    const toasterModel = useContext(ToasterContext);
+    const toasterSpring = useSpring({
+        config: config.gentle,
+        from: toasterFrom,
+        to: !!toasterModel.currentToast ? toasterTo : toasterFrom, 
+    });
 
-  if (!toasterModel.currentToast) return null;
+    if (!toasterModel.currentToast) return null;
 
-  return (
-    <ToasterContainer
-      className={ `${className} ${toasterModel.currentToast.type || ToastType.Success}` }
-      style={ toasterSpring }
-    >
-      { toasterModel.currentToast.message }
-    </ToasterContainer>
-  );
+    return (
+        <ToasterContainer
+            className={ `${className} ${toasterModel.currentToast?.type || ToastType.Success}` }
+            style={ toasterSpring }
+        >
+            <div className='text-container'>
+                { toasterModel.currentToast?.message }
+            </div>
+            <div className='ang' />
+        </ToasterContainer>
+    );
 };
 
 export const Toaster = observer(ToasterBase);
--- src/components/Toaster/styles.tsx
+++ src/components/Toaster/styles.tsx
@@ -1,28 +1,70 @@
 import { animated } from 'react-spring';
 import styled from 'styled-components';
-import { FlexCenter } from '../../styles/styles';
+import { FlexHorizontalCenter } from '../../styles/styles';
 import { IThemeProps } from '../../styles/themes';
 
 export const ToasterContainer = styled(animated.div)<IThemeProps>`
-  ${ FlexCenter }
-  position: absolute;
-  top: 100px;
-  left: 50%;
-  max-width: 350px;
-  padding: 5px 10px;
-  background: ${({theme}) => theme.dark};
-  color: white;
-  z-index: 999;
+    ${ FlexHorizontalCenter }
+    align-items: stretch;
+    position: absolute;
+    top: 20px;
+    right: 20px;
+    z-index: 999;
 
-  &.success {
-    border: ${({theme}) => `1px solid ${theme.primary}`};
-  }
+    &.success {
+        .ang {
+            background: ${({theme}) => theme.primary};
+        }
 
-  &.warning {
-    border: ${({theme}) => `1px solid ${theme.highlight1}`};
-  }
+        .text-container {
+            border: ${({theme}) => `1px solid ${theme.primary}`};
+        }
+    }
 
-  &.error {
-    border: ${({theme}) => `1px solid ${theme.error}`};
-  }
+    &.warning {
+        .ang {
+            background: ${({theme}) => theme.highlight1};
+        }
+
+        .text-container {
+            border: ${({theme}) => `1px solid ${theme.highlight1}`};
+        }
+    }
+
+    &.error {
+        .ang {
+            background: ${({theme}) => theme.error};
+        }
+        
+        .text-container {
+            border: ${({theme}) => `1px solid ${theme.error}`};
+        }
+    }
+
+    .ang {
+        position: relative;
+        width: 10px;
+        margin-left: 2px;
+        clip-path: polygon(0 0, 100% 0, 100% calc(100% - 7px), calc(100% - 7px) 100%, 0 100%);
+
+        &:before {
+            content: ' ';
+            position: absolute;
+            top: 5px;
+            left: 1px;
+            display: block;
+            width: 8px;
+            height: 8px;
+            background: #000;
+        }
+    }
+
+    .text-container {
+        padding: 5px 10px;
+        font-size: 14px;
+        background: ${({theme}) => theme.dark};
+        color: white;
+        white-space: nowrap;
+        overflow: hidden;
+    }
 `;
