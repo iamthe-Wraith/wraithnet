6313ae5bc898d2997750bcb25a8bbd217d2d6ac7
iamthe-Wraith <allhailthewraith@protonmail.com>
2022-01-08 09:55:29 -0500

    updated ref component to use modal instead of popover

--- src/components/Markdown/index.tsx
+++ src/components/Markdown/index.tsx
@@ -1,34 +1,51 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import ReactMarkdown from 'react-markdown';
 import { SpecialComponents } from 'react-markdown/lib/ast-to-react';
 import { NormalComponents } from 'react-markdown/lib/complex-types';
 import { PluggableList } from 'react-markdown/lib/react-markdown';
+import { AnchorComponent } from '../NoteEditor/components/AnchorComponent';
+import { HeadingsComponent } from '../NoteEditor/components/HeadingsComponent';
+import { ParagraphComponent } from '../NoteEditor/components/ParagraphComponent';
+import { RefComponent } from '../NoteEditor/components/RefComponent';
 
 import { MarkdownContainer } from './styles';
 
 interface IProps {
-  className?: string;
-  content: string;
-  rehypePlugins: PluggableList;
-  remarkPlugins: PluggableList;
-  components: Partial<Omit<NormalComponents, keyof SpecialComponents> & SpecialComponents>
+    className?: string;
+    content: string;
+    rehypePlugins: PluggableList;
+    remarkPlugins: PluggableList;
+    components?: Partial<Omit<NormalComponents, keyof SpecialComponents> & SpecialComponents>
 }
 
+const defaultCustomComponents: any = {
+    a: AnchorComponent,
+    p: ParagraphComponent,
+    h1: HeadingsComponent,
+    h2: HeadingsComponent,
+    h3: HeadingsComponent,
+    h4: HeadingsComponent,
+    h5: HeadingsComponent,
+    h6: HeadingsComponent,
+    ref: RefComponent,
+};
+
 export const Markdown: React.FC<IProps> = ({
-  className = '',
-  content,
-  rehypePlugins = [],
-  remarkPlugins = [],
-  components = {},
+    className = '',
+    content,
+    rehypePlugins = [],
+    remarkPlugins = [],
+    components = {},
 }) => {
-  return (
-    <MarkdownContainer className={ className }>
-      <ReactMarkdown
-        children={ content }
-        rehypePlugins={ rehypePlugins }
-        remarkPlugins={ remarkPlugins }
-        components={ components }
-      />
-    </MarkdownContainer>
-  );
+    const customComponents = useRef({ ...defaultCustomComponents, ...components }).current;
+    return (
+        <MarkdownContainer className={ className }>
+            <ReactMarkdown
+                children={ content }
+                rehypePlugins={ rehypePlugins }
+                remarkPlugins={ remarkPlugins }
+                components={ customComponents }
+            />
+        </MarkdownContainer>
+    );
 };
